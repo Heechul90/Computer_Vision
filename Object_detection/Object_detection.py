f5419b06a77aa2780d5b50fb2ca03add59f62428
Heechul90 <heechul4296@gmail.com>
2020-03-24 20:01:18 +0900

    object detection

--- Object_detection/Object_detection.py
+++ Object_detection/Object_detection.py
@@ -54,3 +54,164 @@ cls_pred2 = class_predictor(3, 10)
 cls_pred2.initialize()
 y2 = cls_pred2(feat2)
 print('Class prediction for feature map2', y2.shape)
+
+def flatten_prediction(pred):
+    return nd.flatten(nd.transpose(pred, axes=(0, 2, 3, 1)))
+
+def concat_predictions(preds):
+    return nd.concat(*preds, dim=1)
+
+flat_y1 = flatten_prediction(y1)
+print('Flatten class prediction 1', flat_y1.shape)
+flat_y2 = flatten_prediction(y2)
+print('Flatten class prediction 2', flat_y2.shape)
+print('Concat class predictions', concat_predictions([flat_y1, flat_y2]).shape)
+
+from mxnet import gluon
+def body():
+    """return the body network"""
+    out = nn.HybridSequential()
+    for nfilters in [16, 32, 64]:
+        out.add(down_sample(nfilters))
+    return out
+
+bnet = body()
+bnet.initialize()
+x = nd.zeros((2, 3, 256, 256))
+print('Body network', [y.shape for y in bnet(x)])
+
+def toy_ssd_model(num_anchors, num_classes):
+    """return SSD modules"""
+    downsamples = nn.Sequential()
+    class_preds = nn.Sequential()
+    box_preds = nn.Sequential()
+
+    downsamples.add(down_sample(128))
+    downsamples.add(down_sample(128))
+    downsamples.add(down_sample(128))
+
+    for scale in range(5):
+        class_preds.add(class_predictor(num_anchors, num_classes))
+        box_preds.add(box_predictor(num_anchors))
+
+    return body(), downsamples, class_preds, box_preds
+
+print(toy_ssd_model(5, 2))
+
+
+def toy_ssd_forward(x, body, downsamples, class_preds, box_preds, sizes, ratios):
+    # extract feature with the body network
+    x = body()
+
+    # for each scale, add anchors, box and class predictions
+    # then compute the input to next scale
+    default_anchors = []
+    predicted_boxes = []
+    predicted_classes = []
+
+    for i in range(5):
+        default_anchors.append(MultiBoxPrior(x, sizes=sizes[i], ratios=ratios[i]))
+        predicted_boxes.append(flatten_prediction(box_preds[i](x)))
+        predicted_classes.append(flatten_prediction(class_preds[i](x)))
+        if i < 3:
+            x = downsamples[i](x)
+        elif i == 3:
+            # simply use the pooling layer
+            x = nd.Pooling(x, global_pool=True, pool_type='max', kernel=(4,4))
+
+    return default_anchors, predicted_classes, predicted_boxes
+
+
+from mxnet import gluon
+class ToySSD(gluon.Block):
+    def __init__(self, num_classes, **kwargs):
+        super(ToySSD, self).__init__(**kwargs)
+        # anchor box sizes for 4 feature scales
+        self.anchor_sizes = [[.2, .272], [.37, .447], [.54, .619], [.71, .79], [.88, .961]]
+        # anchor box ratios for 4 feature scales
+        self.anchor_ratios = [[1, 2, .5]] * 5
+        self.num_classes = num_classes
+
+        with self.name_scope():
+            self.body, self.downsamples, self.class_preds, self.box_preds = toy_ssd_model(4, num_classes)
+
+    def forward(self, x):
+        defalt_anchors, predicted_classes, predicted_boxes = toy_ssd_forward(x, self.body, self.downsamples, self.class_preds, self.box_preds, self.anchor_sizes, self.anchor_ratios)
+        # we want to concatenate anchors, class predictions, box predictions from different layers
+        anchors = concat_predictions(defalt_anchors)
+        box_preds = concat_predictions(predicted_boxes)
+        class_preds = concat_predictions(predicted_classes)
+        # it is better to have class predictions reshaped for softmax computation
+        class_preds = nd.reshape(class_preds, shape=(0, -1, self.num_classes + 1))
+
+        return anchors, class_preds, box_preds
+
+net = ToySSD(2)
+net.initialize()
+x = nd.zeros((1, 3, 256, 256))
+default_anchors, class_predictions, box_predictions = net(x)
+print('Outputs:', 'anchors', default_anchors.shape, 'class prediction', class_predictions.shape, 'box prediction', box_predictions.shape)
+
+
+from mxnet.test_utils import download
+import os.path as osp
+
+def verified(file_path, sha1hash):
+    import hashlib
+    sha1 = hashlib.sha1()
+    with open(file_path, 'rb') as f:
+        while True:
+            data = f.read(1048576)
+            if not data:
+                break
+            sha1.update(data)
+    matched = sha1.hexdigest() == sha1hash
+    if not matched:
+        print('Found hash mismatch in file {}, possibly due to incomplete download.'.format(file_path))
+
+    return matched
+
+url_format = 'https://apache-mxnet.s3-accelerate.amazonaws.com/gluon/dataset/pikachu/{}'
+
+hashes = {'train.rec': 'e6bcb6ffba1ac04ff8a9b1115e650af56ee969c8',
+          'train.idx': 'dcf7318b2602c06428b9988470c731621716c393',
+          'val.rec': 'd6c33f799b4d058e82f2cb5bd9a976f69d72d520'}
+
+for k,v in hashes.items():
+    fname = 'pikachu_' + k
+    target = osp.join('data', fname)
+    url = url_format.format(k)
+    if not osp.exists(target) or not verified(target, v):
+        print('Downloading', target, url)
+        download(url, fname=fname, dirname='data', overwrite=True)
+
+import mxnet.image as image
+data_shape = 256
+batch_size = 32
+
+def get_iterators(data_shape, batch_size):
+    class_names = ['pikachu']
+    num_class = len(class_names)
+    train_iter = image.ImageDetIter(
+        batch_size=batch_size,
+        data_shape=(3, data_shape, data_shape),
+        path_imgrec='./data/pikachu_train.rec',
+        path_imgidx='./data/pikachu_train.idx',
+        shuffle=True,
+        mean=True,
+        rand_crop=1,
+        min_object_covered=0.95,
+        max_attempts=200)
+
+    val_iter = image.ImageDetIter(
+        batch_size=batch_size,
+        data_shape=(3, data_shape, data_shape),
+        path_imgrec='./data/pikachu_val.rec',
+        shuffle=False,
+        mean=True)
+
+    return train_iter, val_iter, class_names, num_class
+
+train_data, test_data, class_names, num_class = get_iterators(data_shape, batch_size)
+batch = train_data.next()
+print(batch)
