22643d97f8ea1022ed273ee7a68cabf8d36dd4a6
Heechul90 <heechul4296@gmail.com>
2020-03-25 13:22:54 +0900

    object detection

--- Object_detection/Object_detection.py
+++ Object_detection/Object_detection.py
@@ -1,3 +1,4 @@
+##### Object Detection
 from mxnet.gluon import nn
 import mxnet as mx
 from mxnet import nd
@@ -26,7 +27,7 @@ def down_sample(num_filters):
     """stack two Conv-BatchNorm-Relu blocks and then a pooling layer to halve the feature size"""
     out = nn.HybridSequential()
     for _ in range(2):
-        out.add(nn.Conv2D(num_filters, 3, strides=1, padding=1))
+        out.add(nn.Conv2D(num_filters, kernel_size=3, strides=1, padding=1))
         out.add(nn.BatchNorm(in_channels=num_filters))
         out.add(nn.Activation('relu'))
     out.add(nn.MaxPool2D(2))
@@ -122,7 +123,9 @@ def toy_ssd_forward(x, body, downsamples, class_preds, box_preds, sizes, ratios)
     return default_anchors, predicted_classes, predicted_boxes
 
 
+########################################################################################################################
 from mxnet import gluon
+
 class ToySSD(gluon.Block):
     def __init__(self, num_classes, **kwargs):
         super(ToySSD, self).__init__(**kwargs)
@@ -132,13 +135,17 @@ class ToySSD(gluon.Block):
         self.anchor_ratios = [[1, 2, .5]] * 5
         self.num_classes = num_classes
 
+
         with self.name_scope():
             self.body, self.downsamples, self.class_preds, self.box_preds = toy_ssd_model(4, num_classes)
 
     def forward(self, x):
-        defalt_anchors, predicted_classes, predicted_boxes = toy_ssd_forward(x, self.body, self.downsamples, self.class_preds, self.box_preds, self.anchor_sizes, self.anchor_ratios)
+        default_anchors, predicted_classes, predicted_boxes = toy_ssd_forward(x,
+                                                                              self.body, self.downsamples,
+                                                                              self.class_preds, self.box_preds,
+                                                                              self.anchor_sizes, self.anchor_ratios)
         # we want to concatenate anchors, class predictions, box predictions from different layers
-        anchors = concat_predictions(defalt_anchors)
+        anchors = concat_predictions(default_anchors)
         box_preds = concat_predictions(predicted_boxes)
         class_preds = concat_predictions(predicted_classes)
         # it is better to have class predictions reshaped for softmax computation
@@ -149,7 +156,7 @@ class ToySSD(gluon.Block):
 net = ToySSD(2)
 net.initialize()
 x = nd.zeros((1, 3, 256, 256))
-default_anchors, class_predictions, box_predictions = net(x)
+default_anchors, class_predictions, box_predictions = net.(x)
 print('Outputs:', 'anchors', default_anchors.shape, 'class prediction', class_predictions.shape, 'box prediction', box_predictions.shape)
 
 
@@ -215,3 +222,206 @@ def get_iterators(data_shape, batch_size):
 train_data, test_data, class_names, num_class = get_iterators(data_shape, batch_size)
 batch = train_data.next()
 print(batch)
+
+
+import numpy as np
+
+img = batch.data[0][0].asnumpy()   # grab the first image, convert to numpy array
+img = img.transpose((1, 2, 0))     # we want channel to be the last dimension
+img += np.array([123, 117, 104])
+img = img.astype(np.uint8)         # use uint8 (0-255)
+# draw bounding boxes on image
+for label in batch.label[0][0].asnumpy():
+    if label[0] < 0:
+        break
+    print(label)
+    xmin, ymin, xmax, ymax = [int(x * data_shape) for x in label[1:5]]
+    rect = plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, fill=False, edgecolor=(1, 0, 0), linewidth=3)
+    plt.gca().add_patch(rect)
+plt.imshow(img)
+plt.show()
+
+from mxnet.contrib.ndarray import MultiBoxTarget
+def training_targets(default_anchors, class_predicts, labels):
+    class_predicts = nd.transpose(class_predicts, axes=(0, 2, 1))
+    z = MultiBoxTarget(*[default_anchors, labels, class_predicts])
+    box_target = z[0]   # box offset target for (x, y, width, height)
+    box_mask = z[1]     # mask is used to ignore box offsets we don't want to penalize, e.g. negative samples
+    cls_target = z[2]   # cls_target is an array of labels for all anchors boxes
+
+    return box_target, box_mask, cls_target
+
+class FocalLoss(gluon.loss.Loss):
+    def __init__(self, axis=-1, alpha=0.25, gamma=2, batch_axis=0, **kwargs):
+        super(FocalLoss, self).__init__(None, batch_axis, **kwargs)
+        self._axis = axis
+        self._alpha = alpha
+        self._gamma = gamma
+
+    def hybrid_forward(self, F, output, label):
+        output = F.softmax(output)
+        pt = F.pick(output, label, axis=self._axis, keepdims=True)
+        loss = -self._alpha * ((1 - pt) ** self._gamma) * F.log(pt)
+
+        return F.mean(loss, axis=self._batch_axis, exclude=True)
+
+# cls_loss = gluon.loss.SoftmaxCrossEntropyLoss()
+cls_loss = FocalLoss()
+print(cls_loss)
+
+class SmoothL1Loss(gluon.loss.Loss):
+    def __init__(self, batch_axis=0, **kwargs):
+        super(SmoothL1Loss, self).__init__(None, batch_axis, **kwargs)
+
+    def hybrid_forward(self, F, output, label, mask):
+        loss = F.smooth_l1((output - label) * mask, scalar=10.)
+
+        return F.mean(loss, self._batch_axis, exclude=True)
+
+box_loss = SmoothL1Loss()
+print(box_loss)
+
+cls_metric = mx.metric.Accuracy()
+box_metric = mx.metric.MAE()   # measure absolute differece between prediction and target
+
+### Set context for training
+# ctx = mx.gpu()   # it may takes too long to train using CPU
+ctx = mx.cpu()
+try:
+    _ = nd.zeros(1, ctx=ctx)
+    # pad label for cuda implementation
+    train_data.reshape(label_shape=(3, 5))
+    train_data = test_data.sync_label_shape(train_data)
+except mx.base.MXNetError as err:
+    print('No GPU enabled, fall back to CPU, sit back and be patient...')
+    ctx = mx.cpu()
+
+net.collect_params().reset_ctx(ctx)
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.1, 'wd': 5e-4})
+
+epochs = 150           # set larger to get better performance
+log_interval = 20
+from_scratch = False   # set to True to train from scrach
+if from_scratch:
+    start_epoch = 0
+else:
+    start_epoch = 148
+    pretrained = 'ssd_pretrained.params'
+    sha1 = 'fbb7d872d76355fff1790d864c2238decdb452bc'
+    url = 'https://apache-mxnet.s3-accelerate.amazonaws.com/gluon/models/ssd_pikachu-fbb7d872.params'
+    if not osp.exists(pretrained) or not verified(pretrained, sha1):
+        print('Downloading', pretrained, url)
+        download(url, fname=pretrained, overwrite=True)
+    net.load_params(pretrained, ctx)
+
+
+import time
+from mxnet import autograd as ag
+for epoch in range(start_epoch, epochs):
+    # reset iterator and tick
+    train_data.reset()
+    cls_metric.reset()
+    box_metric.reset()
+    tic = time.time()
+    # iterate through all batch
+    for i, batch in enumerate(train_data):
+        btic = time.time()
+        # record gradients
+        with ag.record():
+            x = batch.data[0].as_in_context(ctx)
+            y = batch.label[0].as_in_context(ctx)
+            default_anchors, class_predictions, box_predictions = net(x)
+            box_target, box_mask, cls_target = training_targets(default_anchors, class_predictions, y)
+            # losses
+            loss1 = cls_loss(class_predictions, cls_target)
+            loss2 = box_loss(box_predictions, box_target, box_mask)
+            # sum all losses
+            loss = loss1 + loss2
+            # backpropagate
+            loss.backward()
+
+        # apply
+        trainer.strp(batch_size)
+
+        # update metrics
+        cls_metric.update([cls_target], [nd.transpose(class_predictions, (0, 2, 1))])
+        box_metric.update([box_target], [box_predictions * box_mask])
+        if (i + 1) % log_interval == 0:
+            name1, val1 = cls_metric.get()
+            name2, val2 = box_metric.get()
+            print('[Epoch %d Batch %d] speed: %f samples/s, training: %s=%f, %s=%f'%(epoch, i, batch_size/(time.time()-btic), name1, val1, name2, val2))
+
+    # end of epoch logging
+    name1, val1 = cls_metric.get()
+    name2, val2 = box_metric.get()
+    print('[Epoch %d] training: %s=%f, %s=%f'%(epoch, name1, val1, name2, val2))
+    print('[Epoch %d] time cost: %f'%(epoch, time.time()-tic))
+
+# we can save the trained parameters to disk
+net.save_params('ssd_%d.params' % epochs)
+
+import numpy as np
+import cv2
+def prepocess(image):
+    """Takes an image and apply preprocess"""
+    # resize to data_shape
+    image = cv2.resize(image, (data_shape, data_shape))
+    # swap BGR to RGB
+    image = image[:, :, (2, 1, 0)]
+    # convert to float before subtracting mean
+    image = image.astype(np.float32)
+    # subtract mean
+    image -= np.array([123, 117, 104])
+    # organize as [batch-channel-height-width]
+    image = np.transpose(image, (2, 0, 1))
+    image = image[np.newaxis, :]
+    # convert to ndarray
+    image = nd.array(image)
+
+    return image
+
+image = cv2.imread('img/pikachu.jpg')
+x = preprocess(image)
+print('x', x.shape)
+
+# if pre-trained model is provided, we can load it
+# net.load_params('ssd_%d.params' % epochs, ctx)
+anchors, cls_preds, box_preds = net(x.as_in_context(ctx))
+print('anchors', anchors)
+print('class predictions', cls_preds)
+print('box delta predictions', box_preds)
+
+from mxnet.contrib.ndarray import MultiBoxDetection
+# convert predictions to probabilities using softmax
+cls_probs = nd.SoftmaxActivation(nd.transpose(cls_preds, (0, 2, 1)), mode='channel')
+# apply shifts to anchors boxes, non-maximum-suppression, etc...
+output = MultiBoxDetection(*[cls_probs, box_preds, anchors], force_suppress=True, clip=False)
+print(output)
+
+def display(img, out, thresh=0.5):
+    import random
+    import matplotlib as mpl
+    mpl.rcParams['figure.figsize'] = (10, 10)
+    pens = dict()
+    plt.clf()
+    plt.imshow()img)
+    for det in out:
+        cid = int(det[0])
+        if cid < 0:
+            continue
+        score = det[1]
+        if score < thresh:
+            continue
+        if cid not in pens:
+            pens[cid] = (random.random(), random.random(), random.random())
+        scales = [img.shape[1], img.shape[0]] * 2
+        xmin, ymin, xmax, ymax = [int(p * s) for p, s in zip(det[2:6].tolist(), scales)]
+        rect = plt.Rectangle((xmin, ymin), xmax - xmin, ymax - ymin, fill=False, edgecolor=pens[cid], linewidth=3)
+        plt.gca().add_patch(rect)
+        text = class_names[cid]
+        plt.gca().text(xmin, ymin - 2, '{:s} {:.3f}'.format(text, score),
+                       bbox = dict(facecolor=pens[cid], alpha=0.5),
+                       fontsize=12, color='white')
+    plt.show()
+
+display(image[:, :, (2, 1, 0)], output[0].asnumpy(), thresh=0.45)
