a68cccadfb560a65330dbfed0b7c72bc9f94bbe6
Heechul90 <heechul4296@gmail.com>
2020-04-06 17:42:43 +0900

    object detection

---
+++ Object_detection_tensorflow/Object_Detection1.py
@@ -0,0 +1,11 @@
+##### TensorFlow Object Detection API를 이용한 다물체 인식하기 Part 1
+
+### 개발 환경
+# OS : Windows 10 64bit
+# Anaconda: 4.8.1 (Python 3.7.3)
+# Spyder: 3.3.3
+# TensorFlow:
+
+
+### 모델 다운로드
+# TensorFlow/Models GitHub 사이트에서 모델을 다운로드 합니다.
